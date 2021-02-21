6551345714cba906224cba34fee6a1072d3d16d1
Luis J Mejia Garcia <ljm36@njit.edu>
2021-02-20 20:46:21 -0500

    Feature: Tested square and square root method with csv

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,7 +1,6 @@
 import unittest
 from Calculator import Calculator
 from CsvReader import CsvReader
-from pprint import pprint
 
 
 class MyTestCase(unittest.TestCase):
@@ -36,16 +35,20 @@ class MyTestCase(unittest.TestCase):
     def test_division_method_calculator(self):
         test_division = CsvReader('/src/Unit Test Division.csv').data
         for row in test_division:
-            self.assertEqual(self.calculator.multiplication(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.division(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_square_method_calculator(self):
-        self.assertEqual(self.calculator.square(5), 25)
-        self.assertEqual(self.calculator.result, 25)
+        test_square = CsvReader('/src/Unit Test Square.csv').data
+        for row in test_square:
+            self.assertEqual(self.calculator.square(row['Value 1']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_root_method_calculator(self):
-        self.assertEqual(self.calculator.root(16), 4)
-        self.assertEqual(self.calculator.result, 4)
+        test_root = CsvReader('/src/Unit Test Square Root.csv').data
+        for row in test_root:
+            self.assertEqual(self.calculator.root(row['Value 1']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
 
 
 if __name__ == '__main__':
