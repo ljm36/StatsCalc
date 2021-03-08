96a89c64ad94ef8126a7f78dc6f33cecc63b3eac
Tarik <ta355@njit.edu>
2021-03-07 20:33:27 -0500

    Task: Add variance to calculator

--- src/calculator/Stats/statsCalc.py
+++ src/calculator/Stats/statsCalc.py
@@ -1,6 +1,7 @@
 from src.calculator.Calculator import Calculator
 from src.calculator.Stats.Mean import mean
 from src.calculator.Stats.SampleMean import sample_mean
+from src.calculator.Stats.Variance import variance
 
 
 class Statistics(Calculator):
@@ -11,4 +12,8 @@ class Statistics(Calculator):
 
     def mean(self, data):
         self.result = sample_mean(data)
+        return self.result
+
+    def variance(self, data):
+        self.result = variance(data)
         return self.result
