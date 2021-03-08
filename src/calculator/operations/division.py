507f3958db9bf01bc2db0012ceaac7c48e0b09b0
Tarik <ta355@njit.edu>
2021-03-07 21:21:03 -0500

    Task: Exception for division

--- src/calculator/operations/division.py
+++ src/calculator/operations/division.py
@@ -1,5 +1,11 @@
 from src.calculator import Calculator
 def division(a, b):
+
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print('Division by 0 error')
+
     return b / a
 
 Calculator.division= staticmethod(Calculator.division)
