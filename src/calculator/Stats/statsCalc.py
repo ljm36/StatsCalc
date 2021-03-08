ec995cfae25fef140e67b8b31023829f5a92f8c1
km759 <km759@njit.edu>
2021-03-07 20:40:15 -0500

    feature: added median function and median to calculator

--- src/calculator/Stats/statsCalc.py
+++ src/calculator/Stats/statsCalc.py
@@ -1,5 +1,6 @@
 from src.calculator.Calculator import Calculator
 from src.calculator.Stats.Mean import mean
+from src.calculator.Stats.Median import median
 from src.calculator.Stats.Mode import mode
 from src.calculator.Stats.Std import std
 
@@ -8,6 +9,12 @@ class Statistics(Calculator):
     def mean(self, data):
         self.result = mean(data)
         return self.result
+
+
+    def median(self, data):
+        self.result = median(data)
+        return self.result
+
 
     def mode(self, data):
         self.result = mode(data)
