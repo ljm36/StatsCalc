108b42bdd9083a012016da1ae157a0724c4998e5
Tarik <ta355@njit.edu>
2021-03-07 21:17:29 -0500

    Task: Create exception for empty list

--- src/calculator/Stats/sample.py
+++ src/calculator/Stats/sample.py
@@ -6,5 +6,14 @@ def getSample():
     for i in range(int(random())):
         n = random.uniform(random(),random())
         random_list.append(n)
+
     return random_list
 
+def exception(random_list):
+
+   if(len(random_list)==0):
+    try:
+        a = random_list
+        return a
+    except:
+        return None
