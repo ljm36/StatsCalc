a01ca544936b7810283812c2742f1deb42db53e0
Luis J Mejia Garcia <ljm36@njit.edu>
2021-03-07 20:28:07 -0500

    Feature: Added mode in calculator

--- src/calculator/Stats/statsCalc.py
+++ src/calculator/Stats/statsCalc.py
@@ -1,10 +1,14 @@
 from src.calculator.Calculator import Calculator
 from src.calculator.Stats.Mean import mean
-
+from src.calculator.Stats.Mode import mode
 
 
 class Statistics(Calculator):
 
     def mean(self, data):
         self.result = mean(data)
+        return self.result
+
+    def mode(self, data):
+        self.result = mode(data)
         return self.result
