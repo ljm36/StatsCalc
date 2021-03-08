15c116156c50b3baa3c6a85867681e394aaa897d
Tarik <ta355@njit.edu>
2021-03-07 20:10:36 -0500

    Fix: Range Generator

--- src/calculator/Stats/sample.py
+++ src/calculator/Stats/sample.py
@@ -1,12 +1,11 @@
 from random import random
 
-
 def getSample(data, sample_size):
     # random_values = random.sample(data, k=sample_size)
     # return random_values
 
     randomlist = []
     for i in range(10):
-        n = random.randint(random(),random())
+        n = random.uniform(random(),random())
         randomlist.append(n)
     return randomlist
