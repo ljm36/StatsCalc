93aff19a842c4e0dc5bb941355721b517a4e2f4f
Tarik <ta355@njit.edu>
2021-03-21 17:42:05 -0400

    Task: Static Method Change

--- src/calculator/Stats/Mean.py
+++ src/calculator/Stats/Mean.py
@@ -1,8 +1,8 @@
 import statistics
 from src.calculator.Stats.sample import getSample
 
-def mean(data):
-    sample = getSample(data)
+def mean(sample):
+    sample = getSample()
     return statistics.mean(sample)
 
 statistics.mean = staticmethod(statistics.mean)
